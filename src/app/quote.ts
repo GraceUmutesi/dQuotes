899e1d21a3cb6b6d819a763fb59fb134748689ee
GraceUmutesi <tesigrace98@gmail.com>
2020-10-25 22:50:20 +0200

    adding dates

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,6 +1,6 @@
 export class Quote {
     showVotes:boolean;
-    constructor(public id: number, public statement: string, public author: string, public senderName: string) {
+    constructor(public id: number, public statement: string, public author: string, public senderName: string, public postDate:Date) {
         this.showVotes=false;
         
     }
--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -8,15 +8,16 @@ import { Quote } from '../quote';
 })
 export class QuoteComponent implements OnInit {
   myQuotes:Quote[] =[
-    new Quote(1,"Your dreams don't have to be lofty; they just have to be lived.","-Kelly Bouchard","Grace"),
-    new Quote(2,"Description begins in the writer’s imagination but should finish in the reader’s.","-Stephen King","Grace"),
-    new Quote(3,"You can’t wait for inspiration. You have to go after it with a club.","-Jack London","Grace"),
-    new Quote(4,"Taking care of yourself makes you stronger for everyone in your life including you.","-Kelly Rudolph","Grace"),
-    new Quote(5,"Faith is taking the first step even when you don't see the full staircase","-Martin Luther King","Grace"),
+    new Quote(1,"Your dreams don't have to be lofty; they just have to be lived.","-Kelly Bouchard","Grace",new Date(2019,1,4)),
+    new Quote(2,"Description begins in the writer’s imagination but should finish in the reader’s.","-Stephen King","Grace",new Date(2018,8,8)),
+    new Quote(3,"You can’t wait for inspiration. You have to go after it with a club.","-Jack London","Grace",new Date(2018,3,8)),
+    new Quote(4,"Taking care of yourself makes you stronger for everyone in your life including you.","-Kelly Rudolph","Grace",new Date(2020,2,10)),
+    new Quote(5,"Faith is taking the first step even when you don't see the full staircase","-Martin Luther King","Grace",new Date(2019,3,27)),
  ];
  addNewQuote(qs){
   let quoteLength = this.myQuotes.length;
   qs.id = quoteLength+1;
+  qs.postDate = new Date(qs.postDate)
   this.myQuotes.push(qs)
 }
 deleteQuote(toDelete,index){
